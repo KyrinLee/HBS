2723ca15d13675fc974cc5dd64e8233071a36dca
VriskaSerket <thealaskarye@gmail.com>
2020-10-27 19:33:57 -0400

    Update HBS.py

--- HBS.py
+++ HBS.py
@@ -277,6 +277,8 @@ def updateEmojiList(message):
 
         oldEmojis = []
         for e in tempEmojis:
+                if message.channel.id == 754527915290525807:
+                            await client.get_channel(754527915290525807).send(str(e))
                 oldEmojis.append(e[0])
 
         tbd = list(sorted(set(oldEmojis) - set(newEmojis)))
