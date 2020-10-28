33f1d30a81bc7be09dfd9ecc0ee77aea09875791
VriskaSerket <thealaskarye@gmail.com>
2020-10-28 00:55:02 -0400

    Update dayCount.py

--- dayCount.py
+++ dayCount.py
@@ -12,6 +12,57 @@ from psycopg2 import Error
 
 DATABASE_URL = os.environ['DATABASE_URL']
 
+
+from string import Formatter
+from datetime import timedelta
+
+def strfdelta(tdelta, fmt='{D:02}d {H:02}h {M:02}m {S:02}s', inputtype='timedelta'):
+    """Convert a datetime.timedelta object or a regular number to a custom-
+    formatted string, just like the stftime() method does for datetime.datetime
+    objects.
+
+    The fmt argument allows custom formatting to be specified.  Fields can 
+    include seconds, minutes, hours, days, and weeks.  Each field is optional.
+
+    Some examples:
+        '{D:02}d {H:02}h {M:02}m {S:02}s' --> '05d 08h 04m 02s' (default)
+        '{W}w {D}d {H}:{M:02}:{S:02}'     --> '4w 5d 8:04:02'
+        '{D:2}d {H:2}:{M:02}:{S:02}'      --> ' 5d  8:04:02'
+        '{H}h {S}s'                       --> '72h 800s'
+
+    The inputtype argument allows tdelta to be a regular number instead of the  
+    default, which is a datetime.timedelta object.  Valid inputtype strings: 
+        's', 'seconds', 
+        'm', 'minutes', 
+        'h', 'hours', 
+        'd', 'days', 
+        'w', 'weeks'
+    """
+
+    # Convert tdelta to integer seconds.
+    if inputtype == 'timedelta':
+        remainder = int(tdelta.total_seconds())
+    elif inputtype in ['s', 'seconds']:
+        remainder = int(tdelta)
+    elif inputtype in ['m', 'minutes']:
+        remainder = int(tdelta)*60
+    elif inputtype in ['h', 'hours']:
+        remainder = int(tdelta)*3600
+    elif inputtype in ['d', 'days']:
+        remainder = int(tdelta)*86400
+    elif inputtype in ['w', 'weeks']:
+        remainder = int(tdelta)*604800
+
+    f = Formatter()
+    desired_fields = [field_tuple[1] for field_tuple in f.parse(fmt)]
+    possible_fields = ('W', 'D', 'H', 'M', 'S')
+    constants = {'W': 604800, 'D': 86400, 'H': 3600, 'M': 60, 'S': 1}
+    values = {}
+    for field in possible_fields:
+        if field in desired_fields and field in constants:
+            values[field], remainder = divmod(remainder, constants[field])
+    return f.format(fmt, **values)
+
 class dayCount(commands.Cog):
     def __init__(self, client):
         self.client = client
@@ -28,7 +79,7 @@ class dayCount(commands.Cog):
 
         try:
             cursor.execute("INSERT INTO counters (name, timestamp, mentions) VALUES (%s, %s, %s)",(counter,currTime,0))
-            await ctx.send("counter " + counter + " created.")
+            await ctx.send("Counter " + counter + " created.")
         except:
             cursor.close()
             connection.close()
@@ -45,7 +96,7 @@ class dayCount(commands.Cog):
             cursor.execute("UPDATE counters SET timestamp=%s, mentions=%s WHERE name=%s",(currTime,mentions,counter))
 
             timeDiff = currTime - timeStamp
-            await ctx.send("counter " + counter + " updated - it has been " + str(timeDiff) + " seconds since this counter was last reset.")
+            await ctx.send("Counter " + counter + " updated - it has been " + strfdelta(timeDiff) + " seconds since this counter was last reset.")
 
         connection.commit()
         cursor.close()
