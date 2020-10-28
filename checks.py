a7705b1aed11722407f8a72dc57d283079378f22
VriskaSerket <thealaskarye@gmail.com>
2020-10-28 19:14:58 -0400

    Update checks.py

--- checks.py
+++ checks.py
@@ -19,7 +19,7 @@ class CheckFailure(commands.CommandError):
 def is_in_guild(guild_id):
     async def predicate(ctx):
         if ctx.guild.id != guild_id:
-            raise CheckFailure("You cannot run this command in this server.")
+            raise CheckFailure(message="You cannot run this command in this server.")
         else:
             return True
     return commands.check(predicate)
