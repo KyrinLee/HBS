191aef7d22b3e69ef15df827f51accaa01fce2e5
VriskaSerket <thealaskarye@gmail.com>
2020-10-28 16:23:15 -0400

    Update HBS.py

--- HBS.py
+++ HBS.py
@@ -31,7 +31,8 @@ def is_in_guild(guild_id):
     async def predicate(ctx):
         if ctx.guild.id != guild_id:
             raise CheckFailure("You cannot run this command in this server.")
-        else return true
+        else:
+            return true
     return commands.check(predicate)
 
 
