e1d5a328c63f7b9210a305795ac780cae14d1d59
VriskaSerket <thealaskarye@gmail.com>
2020-10-29 15:56:55 -0400

    Update Starboards.py

--- Starboards.py
+++ Starboards.py
@@ -18,8 +18,8 @@ class Starboards(commands.Cog):
     def __init__(self, client):
         self.client = client
 
-    @commands.Cog.listener()
-    async def on_member_join(self, member):
+    #@commands.Cog.listener()
+    #async def on_member_join(self, member):
 
     @commands.command(pass_context=True)
     @commands.is_owner()
