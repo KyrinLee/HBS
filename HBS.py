932a2da1e2761bbc20704ce63c395028f931e72e
VriskaSerket <thealaskarye@gmail.com>
2020-11-09 18:07:55 -0500

    Update HBS.py

--- HBS.py
+++ HBS.py
@@ -77,12 +77,12 @@ def splitLongMsg(txt, limit=1990):
 async def on_message(message: discord.Message):
 
 #HANDLE HUSSIEBOT VRISKA REACTS
+    bannedPhrases = ["Good Morning is a valid kid name.", "Fair Enough is a valid kid name.",
+                     "Cool Thanks is a valid kid name."]
     if message.author.id == 480855402289037312: #if hussiebot
-                if (message.content == "<:vriska:480855644388458497>" or message.content == ":vriska:"): 
+                if (message.content == "<:vriska:480855644388458497>" or message.content == ":vriska:" or message.content == ":eye:"): 
                         await message.delete()
-                if (message.content == ":eye:"):
-                        await message.delete()
-                if (message.content == "Good Morning is a valid kid name.") or (message.content == "Fair Enough is a valid kid name."):
+                if (message.content in bannedPhrases:
                         await message.delete()
 
 #EMOJI HANDLING
