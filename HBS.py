c10ea10d22c2e20591dfa267c9e494a657ec79c7
VriskaSerket <thealaskarye@gmail.com>
2020-11-09 16:05:48 -0500

    Update HBS.py

--- HBS.py
+++ HBS.py
@@ -263,8 +263,9 @@ def updateEmojiList(message):
         tbd = list(sorted(set(oldEmojis) - set(newEmojis)))
         tba = list(sorted(set(newEmojis) - set(oldEmojis)))
 
-        await client.get_channel(754527915290525807).send("TBD: " + str(len(tbd)))
-        await client.get_channel(754527915290525807).send("TBA: " + str(len(tbd)))
+        channel = client.get_channel(754527915290525807)
+        await channel.send("TBD: " + str(len(tbd)))
+        await channel.send("TBA: " + str(len(tbd)))
 
         delCount = 0
         addCount = 0
