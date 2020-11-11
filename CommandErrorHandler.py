6efd96a8d9e12667b333de72abf2f636803cf35b
VriskaSerket <thealaskarye@gmail.com>
2020-11-10 22:09:08 -0500

    Update CommandErrorHandler.py

--- CommandErrorHandler.py
+++ CommandErrorHandler.py
@@ -63,7 +63,7 @@ class CommandErrorHandler(commands.Cog):
         elif isinstance(error, commands.NoPrivateMessage):
             try:
                 sys.stdout.write("NPM")
-                await ctx.author.send("Stop tryna slide into my DMs! I'm taken :)")
+                await ctx.send("Stop tryna slide into my DMs! I'm taken :)")
             except discord.HTTPException:
                 pass
 
