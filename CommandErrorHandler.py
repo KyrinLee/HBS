e1dac22a2d2625a181f6da5cb3b436ed305bdce6
VriskaSerket <thealaskarye@gmail.com>
2020-11-10 21:13:01 -0500

    Update CommandErrorHandler.py

--- CommandErrorHandler.py
+++ CommandErrorHandler.py
@@ -68,6 +68,7 @@ class CommandErrorHandler(commands.Cog):
 
         else:
             # All other Errors not returned come here. And we can just print the default TraceBack.
+            await ctx.send("I did a fucky.")
             print('Ignoring exception in command {}:'.format(ctx.command), file=sys.stderr)
             traceback.print_exception(type(error), error, error.__traceback__, file=sys.stderr)
 
