a023838714fd8a2c6ab8d509dccc3d177a054fcf
VriskaSerket <thealaskarye@gmail.com>
2021-04-02 22:08:18 -0400

    Update Birthdays.py

--- cogs/Birthdays.py
+++ cogs/Birthdays.py
@@ -106,7 +106,7 @@ class Birthdays(commands.Cog):
                                 if today.year - birthday.year > 0:
                                     year_text = f': {today.year - birthday.year} years old'
 
-                            output += f'{name} {system.tag} {year_text}\n'
+                            output += f'{name} {system.tag} {year_text}'
 
             if len(output) > 0:
                 output = f'**{re.sub("x","",re.sub("x0","",today.strftime("%B x%d")))} - Today\'s Birthdays:**\n' + output
@@ -115,6 +115,54 @@ class Birthdays(commands.Cog):
         conn.commit()
         cursor.close()
         conn.close()
+
+    @commands.command(pass_context=True)
+    async def todaysBirthdays(self, ctx):
+        async with ctx.channel.typing():
+            output = ""
+            conn = psycopg2.connect(DATABASE_URL, sslmode='require')
+            cursor = conn.cursor()
+
+            today_utc = datetime.now(tz=pytz.utc)
+            today = today_utc.astimezone(timezone('US/Pacific'))
+            
+            cursor.execute("SELECT * FROM pkinfo")
+            data = cursor.fetchall()
+
+            for i in data:
+                try:
+                    async with aiohttp.ClientSession() as session:
+                        system = await pluralKit.System.get_by_hid(session,i[0],i[1])
+                        members = await system.members(session)
+                except:
+                    pass
+                for member in members:
+                    if member.birthday != None and member.visibility != "private" and member.birthday_privacy != "private":
+                        birthday = parser.isoparse(member.birthday)
+                        if birthday.day == today.day and birthday.month == today.month:
+                            if member.name_privacy == "private":
+                                name = member.display_name
+                            else:
+                                name = member.name
+
+                            year_text = ""
+                            if birthday.year != 1 and birthday.year != 4:
+                                if today.year - birthday.year > 0:
+                                    year_text = f': {today.year - birthday.year} years old'
+
+                            output += f'{name} {system.tag} {year_text}\n'
+
+            if len(output) > 0:
+                output = f'**{re.sub("x","",re.sub("x0","",today.strftime("%B x%d")))} - Today\'s Birthdays:**\n' + output
+                output = splitLongMsg(output)
+
+                for o in output:
+                    await self.client.get_channel(754527915290525807).send(o)
+
+            conn.commit()
+            cursor.close()
+            conn.close()
+        
             
     def get_todays_birthdays(self, day):
         birthday_list = []
@@ -225,65 +273,71 @@ class Birthdays(commands.Cog):
 
     @commands.command(pass_context=True, aliases=["myBirthdays", "birthdayList","birthdaysList"])
     async def listBirthdays(self, ctx):
-        output = ""
-        conn = psycopg2.connect(DATABASE_URL, sslmode='require')
-        cursor = conn.cursor()
-        today_utc = datetime.now(tz=pytz.utc)
-        today = today_utc.astimezone(timezone('US/Pacific'))
+        async with ctx.channel.typing():
+            output = ""
+            conn = psycopg2.connect(DATABASE_URL, sslmode='require')
+            cursor = conn.cursor()
+            today_utc = datetime.now(tz=pytz.utc)
+            today = today_utc.astimezone(timezone('US/Pacific'))
 
-        birthdays = []
-        
-        system = await pk.get_system_by_discord_id(ctx.author.id)
-        
-        cursor.execute("SELECT * FROM pkinfo")
-        data = cursor.fetchall()
+            birthdays = []
+            
+            system = await pk.get_system_by_discord_id(ctx.author.id)
+            
+            cursor.execute("SELECT * FROM pkinfo")
+            data = cursor.fetchall()
 
-        for i in data:
-            if system.hid == i[0]:
-                try:
-                    async with aiohttp.ClientSession() as session:
-                        system = await pluralKit.System.get_by_hid(session,i[0],i[1])
-                        members = await system.members(session)
-                except:
-                    raise checks.OtherError("I don't have access to your member list! Either set your member list to public, or run `hbs;deleteSystemBirthdays` and run `hbs;addSystemBirthdays` to share your access token.")
-                
-                for member in members:
-                    if member.birthday != None and member.visibility != "private" and member.birthday_privacy != "private":
-                        birthdays.append(member)
-                        
-                if len(birthdays) > 0:
-                    birthdays.sort(key = lambda d: (parser.isoparse(d.birthday).month, parser.isoparse(d.birthday).day, (d.display_name if d.name_privacy == "private" else d.name)))
-                    for i in range(1,13):
-                        new_birthdays = [b for b in birthdays if parser.isoparse(b.birthday).month == i]
-                        if len(new_birthdays) > 0:
-                            month = date(1900, i, 1).strftime('%B')
-                            output += f'__{month}__\n'
-                            for j in range(1,32):
-                                new_new_birthdays = [b for b in new_birthdays if parser.isoparse(b.birthday).day == j]
-                                if len(new_new_birthdays) > 0:
-                                    day = date(1900, 1, j).strftime('%d').lstrip('0')
-                                    output += f'{day}: '
-                                    
-                                    for member in new_new_birthdays:
-                                        birthday = parser.isoparse(member.birthday)
-
-                                        year_text = ""
-                                        if birthday.year != 1 and birthday.year != 4:
-                                            year_text = f' ({birthday.strftime("%Y")})'
-                                    
-                                        if (member.name_privacy == "private"):
-                                            name = member.display_name
-                                        else:
-                                            name = member.name
-                                            
-                                        output += (f'{name}{year_text}, ')
+            
+            for i in data:
+                if system.hid == i[0]:
+                    try:
+                        async with aiohttp.ClientSession() as session:
+                            system = await pluralKit.System.get_by_hid(session,i[0],i[1])
+                            members = await system.members(session)
+                    except:
+                        raise checks.OtherError("I don't have access to your member list! Either set your member list to public, or run `hbs;deleteSystemBirthdays` and run `hbs;addSystemBirthdays` to share your access token.")
+                    
+                    for member in members:
+                        if member.birthday != None and member.visibility != "private" and member.birthday_privacy != "private":
+                            birthdays.append(member)
+                            
+                    if len(birthdays) > 0:
+                        birthdays.sort(key = lambda d: (parser.parse(d.birthday).month, parser.parse(d.birthday).day, (d.display_name if d.name_privacy == "private" else d.name)))
+                        for i in range(1,13):
+                            new_birthdays = [b for b in birthdays if parser.parse(b.birthday).month == i]
+                            if len(new_birthdays) > 0:
+                                month = date(1900, i, 1).strftime('%B')
+                                output += f'__{month}__\n'
+                                for j in range(1,32):
+                                    new_new_birthdays = [b for b in new_birthdays if parser.parse(b.birthday).day == j]
+                                    if len(new_new_birthdays) > 0:
+                                        day = date(1900, 1, j).strftime('%d').lstrip('0')
+                                        output += f'{day}: '
                                         
-                                    output = output.rstrip(", ")
-                                    output += "\n"
-                        
-        if len(output) > 0:
-            output = f'**Your Birthdays:**\n' + output
-            await ctx.send(output)
+                                        for member in new_new_birthdays:
+                                            birthday = parser.parse(member.birthday)
+
+                                            year_text = ""
+                                            if birthday.year != 1 and birthday.year != 4:
+                                                year_text = f' ({birthday.strftime("%Y")})'
+                                        
+                                            if (member.name_privacy == "private"):
+                                                name = member.display_name
+                                            else:
+                                                name = member.name
+                                                
+                                            output += (f'{name}{year_text}, ')
+                                            
+                                        output = output.rstrip(", ")
+                                        output += "\n"
+                            
+            if len(output) > 0:
+                output = f'**Your Birthdays:**\n' + output
+                output = splitLongMsg(output)
+
+                for o in output:
+                    await ctx.send(o)
+                #await ctx.send(output)
         
     '''@commands.command(pass_context=True)
     async def listBirthdays(self, ctx):
@@ -299,72 +353,74 @@ class Birthdays(commands.Cog):
     @commands.command(pass_context=True)
     @checks.is_vriska()
     async def listAllBirthdays(self, ctx):
-        '''output = "Birthdays: \n"
-        raw_birthdays = gist.files["birthdays.txt"].content
-        raw_birthdays = re.split("\n",raw_birthdays)
-        birthday_list = [Birthday.from_string(birthday) for birthday in raw_birthdays if birthday != ""]
-        for birthday in birthday_list:
-            output += str(birthday) + "\n"
-        await ctx.send(output)'''
+        async with ctx.channel.typing():
+            '''output = "Birthdays: \n"
+            raw_birthdays = gist.files["birthdays.txt"].content
+            raw_birthdays = re.split("\n",raw_birthdays)
+            birthday_list = [Birthday.from_string(birthday) for birthday in raw_birthdays if birthday != ""]
+            for birthday in birthday_list:
+                output += str(birthday) + "\n"
+            await ctx.send(output)'''
 
-        output = ""
-        conn = psycopg2.connect(DATABASE_URL, sslmode='require')
-        cursor = conn.cursor()
-        today_utc = datetime.now(tz=pytz.utc)
-        today = today_utc.astimezone(timezone('US/Pacific'))
+            output = ""
+            conn = psycopg2.connect(DATABASE_URL, sslmode='require')
+            cursor = conn.cursor()
+            today_utc = datetime.now(tz=pytz.utc)
+            today = today_utc.astimezone(timezone('US/Pacific'))
 
-        birthdays = []
-        
-        cursor.execute("SELECT * FROM pkinfo")
-        data = cursor.fetchall()
+            birthdays = []
+            
+            cursor.execute("SELECT * FROM pkinfo")
+            data = cursor.fetchall()
 
-        for i in data:
-            try:
-                async with aiohttp.ClientSession() as session:
-                    system = await pluralKit.System.get_by_hid(session,i[0],i[1])
-                    members = await system.members(session)
-            except:
-                pass
+            for i in data:
+                try:
+                    async with aiohttp.ClientSession() as session:
+                        system = await pluralKit.System.get_by_hid(session,i[0],i[1])
+                        members = await system.members(session)
+                except:
+                    pass
 
-            for member in members:
-                if member.birthday != None and member.visibility != "private" and member.birthday_privacy != "private":
-                    birthdays.append(member)
-                    
-        if len(birthdays) > 0:
-            birthdays.sort(key = lambda d: (parser.isoparse(d.birthday).month, parser.isoparse(d.birthday).day, (d.display_name if d.name_privacy == "private" else d.name)))
-            for i in range(1,13):
-                new_birthdays = [b for b in birthdays if parser.isoparse(b.birthday).month == i]
-                if len(new_birthdays) > 0:
-                    month = date(1900, i, 1).strftime('%B')
-                    output += f'__{month}__\n'
-                    for j in range(1,32):
-                        new_new_birthdays = [b for b in new_birthdays if parser.isoparse(b.birthday).day == j]
-                        if len(new_new_birthdays) > 0:
-                            day = date(1900, 1, j).strftime('%d').lstrip('0')
-                            output += f'{day}: '
-                            
-                            for member in new_new_birthdays:
-                                birthday = parser.isoparse(member.birthday)
-
-                                year_text = ""
-                                if birthday.year != 1 and birthday.year != 4:
-                                    year_text = f' ({birthday.strftime("%Y")})'
-                            
-                                if (member.name_privacy == "private"):
-                                    name = member.display_name
-                                else:
-                                    name = member.name
-                                    
-                                output += (f'{name}{year_text}, ')
-                                
-                            output = output.rstrip(", ")
-                            output += "\n"
+                for member in members:
+                    if member.birthday != None and member.visibility != "private" and member.birthday_privacy != "private":
+                        birthdays.append(member)
                         
-        if len(output) > 0:
-            output = f'**Birthdays:**\n' + output
-            await ctx.send(output)
+            if len(birthdays) > 0:
+                birthdays.sort(key = lambda d: (parser.parse(d.birthday).month, parser.parse(d.birthday).day, (d.display_name if d.name_privacy == "private" else d.name)))
+                for i in range(1,13):
+                    new_birthdays = [b for b in birthdays if parser.isoparse(b.birthday).month == i]
+                    if len(new_birthdays) > 0:
+                        month = date(1900, i, 1).strftime('%B')
+                        output += f'__{month}__\n'
+                        for j in range(1,32):
+                            new_new_birthdays = [b for b in new_birthdays if parser.parse(b.birthday).day == j]
+                            if len(new_new_birthdays) > 0:
+                                day = date(1900, 1, j).strftime('%d').lstrip('0')
+                                output += f'{day}: '
+                                
+                                for member in new_new_birthdays:
+                                    birthday = parser.parse(member.birthday)
 
-        
+                                    year_text = ""
+                                    if birthday.year != 1 and birthday.year != 4:
+                                        year_text = f' ({birthday.strftime("%Y")})'
+                                
+                                    if (member.name_privacy == "private"):
+                                        name = member.display_name
+                                    else:
+                                        name = member.name
+                                        
+                                    output += (f'{name}{year_text}, ')
+                                    
+                                output = output.rstrip(", ")
+                                output += "\n"
+                            
+            if len(output) > 0:
+                output = f'**Birthdays:**\n' + output
+                output = splitLongMsg(output)
+
+                for o in output:
+                    await ctx.send(o)
                        
 def setup(client):
     client.add_cog(Birthdays(client))
