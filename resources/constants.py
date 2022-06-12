8cb71878d4337fa67e9214c70ddf705b6faf9bd8
KyrinLee <kyrin.snyder@gmail.com>
2022-06-12 18:48:55 -0400

    Update constants.py

--- resources/constants.py
+++ resources/constants.py
@@ -63,7 +63,65 @@ bannedPhrases = ["Good Morning", "Good Mornin", "Good Evening", "Good Evenin", "
 starsList = ['｡', '҉', '☆', '°', ':', '✭', '✧', '.', '✼', '✫', '．', '*', '゜', '。', '+', 'ﾟ', '・', '･', '★']
 spaces = [" " for x in range(30)]
 
-who_choices_sky = ["Me.","You.","Andrew Fucking Hussie.","Who do you *think*?","Em'rys.","Prompto.","Hatsune Miku.","Vriska.","Milk Boy.","Nox.","David Elizabeth Strider.","It's a secret.","Why do you gotta know?","That's classified.","I remember, it was many years ago. I was but a young boy..."]
+who_choices_sky = ["Me.","You.","Andrew Fucking Hussie.","Who do you *think*?","Evan/Em.","Prompto.","Hatsune Miku.","Vriska.",
+                   "Milk Boy.","Nox.","David Elizabeth Strider.","It's a secret.","Why do you gotta know?",
+                   "That's classified.","I remember, it was many years ago. I was but a young boy...",
+                   "I refuse to answer.", "shut up.", "I hate answering your stupid questions.", "Why are you like this?",
+                   "Buy me chicken nuggets and I'll tell you.",
+                   """We're no strangers to love
+                    You know the rules and so do I (do I)
+                    A full commitment's what I'm thinking of
+                    You wouldn't get this from any other guy
+                    I just wanna tell you how I'm feeling
+                    Gotta make you understand
+                    Never gonna give you up
+                    Never gonna let you down
+                    Never gonna run around and desert you
+                    Never gonna make you cry
+                    Never gonna say goodbye
+                    Never gonna tell a lie and hurt you
+                    We've known each other for so long
+                    Your heart's been aching, but you're too shy to say it (say it)
+                    Inside, we both know what's been going on (going on)
+                    We know the game and we're gonna play it
+                    And if you ask me how I'm feeling
+                    Don't tell me you're too blind to see
+                    Never gonna give you up
+                    Never gonna let you down
+                    Never gonna run around and desert you
+                    Never gonna make you cry
+                    Never gonna say goodbye
+                    Never gonna tell a lie and hurt you
+                    Never gonna give you up
+                    Never gonna let you down
+                    Never gonna run around and desert you
+                    Never gonna make you cry
+                    Never gonna say goodbye
+                    Never gonna tell a lie and hurt you
+                    We've known each other for so long
+                    Your heart's been aching, but you're too shy to say it (to say it)
+                    Inside, we both know what's been going on (going on)
+                    We know the game and we're gonna play it
+                    I just wanna tell you how I'm feeling
+                    Gotta make you understand
+                    Never gonna give you up
+                    Never gonna let you down
+                    Never gonna run around and desert you
+                    Never gonna make you cry
+                    Never gonna say goodbye
+                    Never gonna tell a lie and hurt you
+                    Never gonna give you up
+                    Never gonna let you down
+                    Never gonna run around and desert you
+                    Never gonna make you cry
+                    Never gonna say goodbye
+                    Never gonna tell a lie and hurt you
+                    Never gonna give you up
+                    Never gonna let you down
+                    Never gonna run around and desert you
+                    Never gonna make you cry
+                    Never gonna say goodbye
+                    Never gonna tell a lie and hurt you"""]
 who_choices = ["Me.","You.","Andrew Fucking Hussie.","Who do you *think*?","Hatsune Miku.","Vriska.","It's a secret.","Why do you gotta know?","That's classified.","I remember, it was many years ago. I was but a young boy..."]
 
 DATABASE_URL = os.environ['DATABASE_URL']
